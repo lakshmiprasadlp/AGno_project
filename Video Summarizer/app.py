e2a0672ec683c778643d49e760dffa387a66ccde
lakshmiprasadlp <145165461+lakshmiprasadlp@users.noreply.github.com>
2025-08-14 17:47:53 +0530

    Update app.py

--- Video Summarizer/app.py
+++ Video Summarizer/app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
-from phi.agent import Agent
-from phi.model.google import Gemini
-from phi.tools.duckduckgo import DuckDuckGo
+from agno.agent import Agent
+from agno.model.google import Gemini
+from agno.tools.duckduckgo import DuckDuckGo
 from google.generativeai import upload_file,get_file
 import google.generativeai as genai
 
